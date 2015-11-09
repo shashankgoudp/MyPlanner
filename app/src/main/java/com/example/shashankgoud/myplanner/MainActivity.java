b2a10f79271201397bc81834b3f28770d08081a6
shashank goud <shashankgoudp@gmail.com>
2015-11-08 19:40:06 -0500

    Adding complete project

---
+++ app/src/main/java/com/example/shashankgoud/myplanner/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.shashankgoud.myplanner;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void buttonOnClick(View v)
+    {
+
+//        Button button = (Button) v;
+//        ((Button) v).setText("Signup");
+
+        Log.i("clicks", "You Clicked B1");
+        Intent i=new Intent(MainActivity.this, HomeScreenActivity.class);
+        startActivity(i);
+
+    }
+
+
+    public void onClick(View v)
+    {
+
+        TextView textView = (TextView)v;
+        ((TextView) v).setText("It worked");
+
+    }
+}
